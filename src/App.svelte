bd587b761eea0e205633facbe4d3c2749be4fd94
b3b00 <olivier.duhart@gmail.com>
2024-02-12 17:57:07 +0100

    initial

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+
+	import TaskRenderer from "./TaskRenderer.svelte";
+	import SvelteMarkdown from "svelte-markdown";
+
+
+	// let content:string = `
+	// - [ ] todo
+	// - [X] done
+	// - [ ] nested tasks
+	// 	- [X] sub task #1
+	// 	- [ ] sub task #2
+	// 	- [X] sub task #3
+
+	// `
+
+	let content = `**toto**
+- [ ] todo
+- [X] done
+- [ ] nested tasks
+    - [X] sub task #1
+    - [ ] sub task #2
+    - [X] sub task #3		
+`;
+	
+	
+</script>
+
+<SvelteMarkdown renderers={{listitem: TaskRenderer	}} source={content} />
+
+
